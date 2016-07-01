1398af13125b36ec472960c5cf840ffec7ed5a2e
Dave Landry <landry.dave@gmail.com>
2016-07-01 17:03:38 -0400

    udpates eslint

--- src/defaults.js
+++ src/defaults.js
@@ -14,12 +14,12 @@ import {scaleOrdinal} from "d3-scale";
       * | scale | `scale.ordinal().range([ "#b22200", "#eace3f", "#282f6b", "#b35c1e", "#224f20", "#5f487c", "#759143", "#419391", "#993c88", "#e89c89", "#ffee8d", "#afd5e8", "#f7ba77", "#a5c697", "#c5b5e5", "#d1d392", "#bbefd0", "#e099cf"])` | An ordinal scale used in the [assign](#assign) function for non-valid color strings and numbers. |
 */
 const defaults = {
-  "dark": "#444444",
-  "light": "#f7f7f7",
-  "missing": "#cccccc",
-  "off": "#b22200",
-  "on": "#224f20",
-  "scale": scaleOrdinal().range([
+  dark: "#444444",
+  light: "#f7f7f7",
+  missing: "#cccccc",
+  off: "#b22200",
+  on: "#224f20",
+  scale: scaleOrdinal().range([
     "#b22200", "#282f6b", "#eace3f", "#b35c1e", "#224f20", "#5f487c",
     "#759143", "#419391", "#993c88", "#e89c89", "#ffee8d", "#afd5e8",
     "#f7ba77", "#a5c697", "#c5b5e5", "#d1d392", "#bbefd0", "#e099cf"
