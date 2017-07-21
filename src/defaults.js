93c1680733a6097ef11dd2885918eea766db5f62
Dave Landry <landry.dave@gmail.com>
2017-07-21 18:17:33 -0400

    changes defaults docs from a module to a namespace

--- src/defaults.js
+++ src/defaults.js
@@ -1,7 +1,7 @@
 import {scaleOrdinal} from "d3-scale";
 
 /**
-    @module {Object} colorDefaults
+    @namespace {Object} colorDefaults
     @desc A set of default color values used when assigning colors based on data.
       *
       * | Name | Default | Description |
@@ -11,7 +11,7 @@ import {scaleOrdinal} from "d3-scale";
       * | missing | #cccccc | Used in the [assign](#assign) function when the value passed is `null` or `undefined`. |
       * | off | #b22200 | Used in the [assign](#assign) function when the value passed is `false`. |
       * | on | #224f20 | Used in the [assign](#assign) function when the value passed is `true`. |
-      * | scale | `scale.ordinal().range([ "#b22200", "#eace3f", "#282f6b", "#b35c1e", "#224f20", "#5f487c", "#759143", "#419391", "#993c88", "#e89c89", "#ffee8d", "#afd5e8", "#f7ba77", "#a5c697", "#c5b5e5", "#d1d392", "#bbefd0", "#e099cf"])` | An ordinal scale used in the [assign](#assign) function for non-valid color strings and numbers. |
+      * | scale | #b22200, #eace3f, #282f6b, #b35c1e, #224f20, #5f487c, #759143, #419391, #993c88, #e89c89, #ffee8d, #afd5e8, #f7ba77, #a5c697, #c5b5e5, #d1d392, #bbefd0, #e099cf | An ordinal scale used in the [assign](#assign) function for non-valid color strings and numbers. |
 */
 const defaults = {
   dark: "#444444",
