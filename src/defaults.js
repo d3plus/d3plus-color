ce9e2a467cb55c82dd8495da0b90ab6727e51e44
Dave Landry <landry.dave@gmail.com>
2021-02-02 09:24:16 -0500

    updates default color defaults

--- src/defaults.js
+++ src/defaults.js
@@ -6,23 +6,38 @@ import {scaleOrdinal} from "d3-scale";
       *
       * | Name | Default | Description |
       * |---|---|---|
-      * | dark | #444444 | Used in the [contrast](#contrast) function when the color given is very light. |
-      * | light | #f7f7f7 | Used in the [contrast](#contrast) function when the color given is very dark. |
-      * | missing | #cccccc | Used in the [assign](#assign) function when the value passed is `null` or `undefined`. |
-      * | off | #b22200 | Used in the [assign](#assign) function when the value passed is `false`. |
-      * | on | #224f20 | Used in the [assign](#assign) function when the value passed is `true`. |
-      * | scale | #b22200, #eace3f, #282f6b, #b35c1e, #224f20, #5f487c, #759143, #419391, #993c88, #e89c89, #ffee8d, #afd5e8, #f7ba77, #a5c697, #c5b5e5, #d1d392, #bbefd0, #e099cf | An ordinal scale used in the [assign](#assign) function for non-valid color strings and numbers. |
+      * | dark | "#555555" | Used in the [contrast](#contrast) function when the color given is very light. |
+      * | light | "#f7f7f7" | Used in the [contrast](#contrast) function when the color given is very dark. |
+      * | missing | "#cccccc" | Used in the [assign](#assign) function when the value passed is `null` or `undefined`. |
+      * | off | "#C44536" | Used in the [assign](#assign) function when the value passed is `false`. |
+      * | on | "#6A994E" | Used in the [assign](#assign) function when the value passed is `true`. |
+      * | scale | "#4281A4", "#F6AE2D", "#C44536", "#2A9D8F", "#6A994E", "#CEB54A", "#5E548E", "#C08497", "#99582A", "#8C8C99", "#1D3557", "#D08C60", "#6D2E46", "#8BB19C", "#52796F", "#5E60CE", "#985277", "#5C374C" | An ordinal scale used in the [assign](#assign) function for non-valid color strings and numbers. |
 */
 const defaults = {
-  dark: "#444444",
+  dark: "#555555",
   light: "#f7f7f7",
   missing: "#cccccc",
-  off: "#b22200",
-  on: "#224f20",
+  off: "#C44536",
+  on: "#6A994E",
   scale: scaleOrdinal().range([
-    "#b22200", "#282f6b", "#eace3f", "#b35c1e", "#224f20", "#5f487c",
-    "#759143", "#419391", "#993c88", "#e89c89", "#ffee8d", "#afd5e8",
-    "#f7ba77", "#a5c697", "#c5b5e5", "#d1d392", "#bbefd0", "#e099cf"
+    "#4281A4",
+    "#F6AE2D",
+    "#C44536",
+    "#2A9D8F",
+    "#6A994E",
+    "#CEB54A",
+    "#5E548E",
+    "#C08497",
+    "#99582A",
+    "#8C8C99",
+    "#1D3557",
+    "#D08C60",
+    "#6D2E46",
+    "#8BB19C",
+    "#52796F",
+    "#5E60CE",
+    "#985277",
+    "#5C374C"
   ])
 };
 
