b1c897df10d64f55dc29223e9538d35f90653ea6
Dave Landry <landry.dave@gmail.com>
2023-01-24 19:09:06 -0500

    fixes module exports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export {default as colorAdd} from "./src/add";
-export {default as colorAssign} from "./src/assign";
-export {default as colorContrast} from "./src/contrast";
-export {default as colorDefaults} from "./src/defaults";
-export {default as colorLegible} from "./src/legible";
-export {default as colorLighter} from "./src/lighter";
-export {default as colorSubtract} from "./src/subtract";
+export {default as colorAdd} from "./src/add.js";
+export {default as colorAssign} from "./src/assign.js";
+export {default as colorContrast} from "./src/contrast.js";
+export {default as colorDefaults} from "./src/defaults.js";
+export {default as colorLegible} from "./src/legible.js";
+export {default as colorLighter} from "./src/lighter.js";
+export {default as colorSubtract} from "./src/subtract.js";
